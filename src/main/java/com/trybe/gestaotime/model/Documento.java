439da8363524e13a1ae5c7bef4bff68ee06ff59c
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-09 18:54:32 -0300

    Implementação: Cria Classe de entidade Documento

--- src/main/java/com/trybe/gestaotime/model/Documento.java
+++ src/main/java/com/trybe/gestaotime/model/Documento.java
@@ -1,16 +1,52 @@
 package com.trybe.gestaotime.model;
 
-/**
- * Classe Documento.
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+/**Classe Documento.*/
+@Entity
 public class Documento {
-  /**
-   * Atributos.
-   */
 
-  /**
-   * Métodos.
-   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  private String cpf;
+  
+  @Column(name = "numero_carteira_trabalho")
+  private String numeroCarteiraTrabalho;
+  
+  @Column(name = "numero_cbf")
+  private String numeroCbf;
 
+  public Long getId() {
+    return id;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getNumeroCarteiraTrabalho() {
+    return numeroCarteiraTrabalho;
+  }
+
+  public void setNumeroCarteiraTrabalho(String numeroCarteiraTrabalho) {
+    this.numeroCarteiraTrabalho = numeroCarteiraTrabalho;
+  }
+
+  public String getNumeroCbf() {
+    return numeroCbf;
+  }
+
+  public void setNumeroCbf(String numeroCbf) {
+    this.numeroCbf = numeroCbf;
+  }
 }
