2eebd9e2e0c551d84d0026a0919f0acbca243fd0
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-09 18:57:26 -0300

    Implementação: Cria Classe de entidade Torcedor

--- src/main/java/com/trybe/gestaotime/model/Torcedor.java
+++ src/main/java/com/trybe/gestaotime/model/Torcedor.java
@@ -1,17 +1,47 @@
 package com.trybe.gestaotime.model;
 
-/**
- * Classe Torcedor.
- **/
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
+/**Classe Torcedor.*/
+@Entity
 public class Torcedor {
 
-  /**
-   * Atributos.
-   **/
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  private String nome;
+  
+  @ManyToMany(mappedBy = "torcedores")
+  private List<Time> times;
+  
+  public Torcedor() {
+    this.times = new ArrayList<Time>();
+  }
 
-  /**
-   * Métodos.
-   **/
+  public Long getId() {
+    return id;
+  }
+  
+  public String getNome() {
+    return nome;
+  }
 
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public List<Time> getTimes() {
+    return times;
+  }
+
+  public void setTimes(Time time) {
+    this.times.add(time);
+  }
 }
