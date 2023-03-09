deb6483523fd5e9c9796fec8ba06c4a9e5c19fc7
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-09 18:57:05 -0300

    Implementação: Cria Classe de entidade Time

--- src/main/java/com/trybe/gestaotime/model/Time.java
+++ src/main/java/com/trybe/gestaotime/model/Time.java
@@ -1,17 +1,75 @@
 package com.trybe.gestaotime.model;
 
-/**
- * Classe Time.
- **/
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
+/**Classe Time.*/
+@Entity
 public class Time {
 
-  /**
-   * Atributos.
-   **/
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  private String nome;
+  
+  @OneToMany(mappedBy = "time", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Jogador> jogadores;
+  
+  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+  @JoinTable(name = "time_torcedor",
+      joinColumns = @JoinColumn(name = "time_id"),
+      inverseJoinColumns = @JoinColumn(name = "torcedor_id"))
+  private List<Torcedor> torcedores;
+  
+  public Time() {
+    this.jogadores = new ArrayList<Jogador>();
+    this.torcedores = new ArrayList<Torcedor>();
+  }
+  
+  public Long getId() {
+    return id;
+  }
+  
+  public String getNome() {
+    return nome;
+  }
 
-  /**
-   * Métodos.
-   **/
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
+  public List<Jogador> getJogadores() {
+    return jogadores;
+  }
+  
+  public void setJogadores(List<Jogador> jogadores) {
+    this.jogadores = jogadores;
+  }
+
+  public void addJogadores(Jogador jogador) {
+    this.jogadores.add(jogador);
+  }
+
+  public List<Torcedor> getTorcedores() {
+    return torcedores;
+  }
+  
+  public void setTorcedores(List<Torcedor> torcedores) {
+    this.torcedores = torcedores;
+  }
+
+  public void addTorcedores(Torcedor torcedor) {
+    this.torcedores.add(torcedor);
+  }
 }
