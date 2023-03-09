728121c2f19073c09bf670b742aa2ab11e481b53
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-09 19:28:00 -0300

    Implementação: Cria Classe DAO TimeDao

--- src/main/java/com/trybe/gestaotime/dao/TimeDao.java
+++ src/main/java/com/trybe/gestaotime/dao/TimeDao.java
@@ -2,6 +2,10 @@ package com.trybe.gestaotime.dao;
 
 import com.trybe.gestaotime.model.Time;
 
+/**Classe TimeDao que estende GenericDao. */
 public class TimeDao extends GenericDao<Time, Integer> {
 
+  public TimeDao() {
+    super(Time.class);
+  }
 }
