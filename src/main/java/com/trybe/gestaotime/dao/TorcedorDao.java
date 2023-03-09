3a9b600dc2670d5d0433ed0f14353b0132a35794
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-09 19:28:23 -0300

    Implementação: Cria Classe DAO TorcedorDao

--- src/main/java/com/trybe/gestaotime/dao/TorcedorDao.java
+++ src/main/java/com/trybe/gestaotime/dao/TorcedorDao.java
@@ -2,6 +2,10 @@ package com.trybe.gestaotime.dao;
 
 import com.trybe.gestaotime.model.Torcedor;
 
+/**Classe TorcedorDao. */
 public class TorcedorDao extends GenericDao<Torcedor, Integer> {
 
+  public TorcedorDao() {
+    super(Torcedor.class);
+  }
 }
