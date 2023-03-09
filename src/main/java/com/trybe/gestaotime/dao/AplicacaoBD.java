951c22f9cd0fb5d50ef3ed5eb1103af592e10f7f
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-09 19:29:00 -0300

    Implementação: Cria Classe AplicacaoBd

--- src/main/java/com/trybe/gestaotime/dao/AplicacaoBD.java
+++ src/main/java/com/trybe/gestaotime/dao/AplicacaoBD.java
@@ -1,18 +1,33 @@
 package com.trybe.gestaotime.dao;
 
-/**
- * Classe AplicacaoBD.
- **/
-
+/** Classe AplicacaoBd. */
 public class AplicacaoBD {
+  private TimeDao timeDao;
+  private JogadorDao jogadorDao;
+  private TorcedorDao torcedorDao;
+  private DocumentoDao documentoDao;
 
-  /**
-   * Atributos.
-   **/
+  /**Construtor da classe AplicacaoBd.*/
+  public AplicacaoBD() {
+    this.timeDao = new TimeDao();
+    this.jogadorDao = new JogadorDao();
+    this.torcedorDao = new TorcedorDao();
+    this.documentoDao = new DocumentoDao();
+  }
 
+  public TimeDao timeDao() {
+    return timeDao;
+  }
 
-  /**
-   * Métodos.
-   **/
+  public JogadorDao jogadorDao() {
+    return jogadorDao;
+  }
 
+  public TorcedorDao torcedorDao() {
+    return torcedorDao;
+  }
+
+  public DocumentoDao documentoDao() {
+    return documentoDao;
+  }
 }
