d1ed1985d1b52f6f82305b92e5aea8d71cbec126
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-09 19:27:32 -0300

    Implementação: Cria Classe DAO JogadorDao

--- src/main/java/com/trybe/gestaotime/dao/JogadorDao.java
+++ src/main/java/com/trybe/gestaotime/dao/JogadorDao.java
@@ -2,6 +2,10 @@ package com.trybe.gestaotime.dao;
 
 import com.trybe.gestaotime.model.Jogador;
 
+/**Classe JogadorDao que estende GenericDao. */
 public class JogadorDao extends GenericDao<Jogador, Integer> {
 
+  public JogadorDao() {
+    super(Jogador.class);
+  }
 }
