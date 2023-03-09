b2f54480748f7ec8648928982ed523194de48494
Ana Luisa <ana.luisa.isa@hotmail.com>
2023-03-09 19:27:09 -0300

    Implementação: Cria Classe DAO DocumentoDao

--- src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
+++ src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
@@ -2,6 +2,10 @@ package com.trybe.gestaotime.dao;
 
 import com.trybe.gestaotime.model.Documento;
 
+/**Classe DocumentoDao que estende GenericDao. */
 public class DocumentoDao extends GenericDao<Documento, Integer> {
 
+  public DocumentoDao() {
+    super(Documento.class);
+  }
 }
